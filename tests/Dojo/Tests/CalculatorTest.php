c6195622dd7dc877bda102356747fedc38465219
Nico Hiort af Ornäs <nico@allproperty.com.sg>
2013-03-12 15:55:10 +0800

    Nico was here. Refactored tests.

--- tests/Dojo/Tests/CalculatorTest.php
+++ tests/Dojo/Tests/CalculatorTest.php
@@ -6,11 +6,14 @@ use Dojo\Calculator;
 
 class CalculatorTest extends \PHPUnit_Framework_TestCase
 {
+	
+	public function setup()
+	{
+		$this->calculator = new Calculator();
+	}
+	
     public function testDisplaysZeroWhenEnteringZero()
     {
-        // Arrange
-        $this->calculator = new Calculator();
-
         // Act
         $this->calculator->enter('0');
 
@@ -22,21 +25,18 @@ class CalculatorTest extends \PHPUnit_Framework_TestCase
 	
 	public function testAddOneAndOneEqualsTwo()
 	{
-		$this->calculator = new Calculator();
 		$this->calculator->add('1','1');
 		$this->assertEquals('2', $this->calculator->display());
 	}
 	
 	public function testAddTwoAndOneEqualsThree()
 	{
-		$this->calculator = new Calculator();
 		$this->calculator->add('2','1');
 		$this->assertEquals('3', $this->calculator->display());
 	}
 	
 	public function testAddVeryBigNumberAndOneEqualsVeryBigNumberPlusOne()
 	{
-		$this->calculator = new Calculator();
 		$this->calculator->add('2222222222222222','1');
 		$this->assertEquals('2222222222222223', $this->calculator->display());
 	}
