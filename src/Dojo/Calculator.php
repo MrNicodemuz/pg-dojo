cfa4ac15f6fb48839c327a71191f8852e1098a84
Nico Hiort af Ornäs <nico@allproperty.com.sg>
2013-03-12 16:08:17 +0800

    Scott fixed coding standard issues

--- src/Dojo/Calculator.php
+++ src/Dojo/Calculator.php
@@ -4,17 +4,20 @@ namespace Dojo;
 
 class Calculator
 {
-	private $result;
-	
-	public function enter($input) {
-		$this->result = $input;
-	}
-	
-	public function display() {
-		return $this->result;
-	}
-	
-	public function add($number1, $number2) {
-		$this->result = $number1 + $number2;
-	}
+    private $result;
+
+    public function enter($input)
+    {
+        $this->result = $input;
+    }
+
+    public function display()
+    {
+        return $this->result;
+    }
+
+    public function add($number1, $number2)
+    {
+        $this->result = $number1 + $number2;
+    }
 }
--- tests/Dojo/Tests/CalculatorTest.php
+++ tests/Dojo/Tests/CalculatorTest.php
@@ -6,12 +6,12 @@ use Dojo\Calculator;
 
 class CalculatorTest extends \PHPUnit_Framework_TestCase
 {
-	
-	public function setup()
-	{
-		$this->calculator = new Calculator();
-	}
-	
+
+    public function setup()
+    {
+        $this->calculator = new Calculator();
+    }
+
     public function testDisplaysZeroWhenEnteringZero()
     {
         // Act
@@ -22,22 +22,22 @@ class CalculatorTest extends \PHPUnit_Framework_TestCase
         $actual = $this->calculator->display();
         $this->assertEquals($expected, $actual);
     }
-	
-	/**
-	 * @dataProvider dataProvider
-	 */
-	public function testAddMethod($number1, $number2, $expected)
-	{
-		$this->calculator->add($number1, $number2);
-		$this->assertEquals($expected, $this->calculator->display());
-	}
-	
-	public function dataProvider() 
-	{
-		return array(
-			array('1', '1', '2'),
-			array('2', '1', '3'),
-			array('2222222222222222', '1', '2222222222222223')
-		);
-	}
+
+    /**
+     * @dataProvider dataProvider
+     */
+    public function testAddMethod($number1, $number2, $expected)
+    {
+        $this->calculator->add($number1, $number2);
+        $this->assertEquals($expected, $this->calculator->display());
+    }
+
+    public function dataProvider()
+    {
+        return array(
+            array('1', '1', '2'),
+            array('2', '1', '3'),
+            array('2222222222222222', '1', '2222222222222223')
+        );
+    }
 }
